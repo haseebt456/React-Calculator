c5337382ff5894b3b06bd107fae74a45144c43b6
haseebt456 <m.haseebtufail@gmail.com>
2023-12-16 23:23:52 +0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import './App.css';
-
+import ButtonsPanel from './components/ButtonsPanel';
+import DisplayResult from './components/DisplayResult';
 function App() {
   return (
     <>
+    <DisplayResult/>
+    <ButtonsPanel/>
     </>
   );
 }
---
+++ src/components/DisplayResult.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DisplayResult() {
+  return (
+    <div>DisplayResult</div>
+  )
+}
