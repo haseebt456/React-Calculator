3a18204e437488389153c68bea3f9e3d2121ff78
haseebt456 <m.haseebtufail@gmail.com>
2023-12-17 01:27:06 +0500

    Handle calculator styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import './button.css';
 
 export const Button = ({name,color,wide}) => {
     const buttonStyle = {
-        backgroundColor: color || 'orange', // Default to orange if color prop is not provided
+        backgroundColor: color || '#FF9500', // Default to orange if color prop is not provided
       };
   return (
     <button className={wide===true? 'wide':''} style={buttonStyle}>{name}</button>
--- src/components/ButtonsPanel.jsx
+++ src/components/ButtonsPanel.jsx
@@ -6,32 +6,32 @@ export default function ButtonsPanel() {
     <>
     <div id="button-pannel">
     <div className='group1'>
-    <Button name="AC"/>
-    <Button name="+/-"/>
-    <Button name="%"/>
+    <Button name="AC" color=' #E4E4E4'/>
+    <Button name="+/-"color=' #E4E4E4'/>
+    <Button name="%"color=' #E4E4E4'/>
     <Button name="/"/>
     </div>
     <div className='group2'>
-    <Button name="7"/>
-    <Button name="8"/>
-    <Button name="9"/>
+    <Button name="7"color='#E4E4E4'/>
+    <Button name="8"color='#E4E4E4'/>
+    <Button name="9"color='#E4E4E4'/>
     <Button name="X"/>
     </div>
     <div className='group3'>
-    <Button name="4"/>
-    <Button name="5"/>
-    <Button name="6"/>
+    <Button name="4"color='#E4E4E4'/>
+    <Button name="5"color='#E4E4E4'/>
+    <Button name="6" color='#E4E4E4'/>
     <Button name="-"/>
     </div>
     <div className='group4'>
-    <Button name="1"/>
-    <Button name="2"/>
-    <Button name="3"/>
+    <Button name="1" color='#E4E4E4'/>
+    <Button name="2" color='#E4E4E4'/>
+    <Button name="3" color='#E4E4E4'/>
     <Button name="+"/>
     </div>
     <div className='group5'>
-        <Button name="0" wide={true}/>
-        <Button name="."/>
+        <Button name="0" wide={true} color='#E4E4E4'/>
+        <Button name="." color='#E4E4E4'/>
         <Button name="="/>
     </div>
     </div>
